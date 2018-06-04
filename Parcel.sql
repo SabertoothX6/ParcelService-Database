db491d04398a27203a2f03027b6a11bbe0da4baf
SabertoothX6 <sabertoothx6@hotmail.de>
2018-06-04 16:33:07 +0200

    Added Create User commands

--- Parcel.sql
+++ Parcel.sql
@@ -14,6 +14,11 @@ CREATE TABLE `ParcelSize` (
 LOCK TABLES `ParcelSize` WRITE;
 INSERT INTO `ParcelSize` VALUES (1,'XS',0,35),(2,'S',35,50),(3,'M',50,65),(4,'L',65,80),(5,'XL',80,300);
 
+CREATE USER 'reader'@'localhost' IDENTIFIED BY 'penis';
+CREATE USER 'reader'@'%' IDENTIFIED BY 'penis';
+
+GRANT ALL ON *.* TO 'reader'@'localhost';
+GRANT ALL ON *.* TO 'reader'@'%';
 
 GRANT SELECT ON `ParcelService`.* TO 'reader'@'%' IDENTIFIED BY 'penis';
 FLUSH PRIVILEGES;
